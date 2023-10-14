f2ed62aabfad73a71a65efc35a64f17274976ab5
Leo Kaiser <leoka@LeoK-PC>
2023-10-14 15:53:08 -0500

    hiding api key and other stuff

--- backend/initializeCustomer.py
+++ backend/initializeCustomer.py
@@ -1,7 +1,13 @@
 # functions for creating sameple customer, still need to make on function to do all of this, I might put it all in a class. 
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv("key.env")
 
-API_KEY = '344fcb27caa92be8e7b7c26b5d230022'
+API_KEY = os.getenv("API_KEY")
+print(API_KEY)
+
+
 url = 'https://api.finicity.com/aggregation/v2/partners/authentication'
 partnerId = "2445584332755"
 partnerSecret = "Hh4x0opoX0xfvN4oJ15y"
@@ -31,30 +37,32 @@ def getToken(key, secret, id, url):
         return None
 
 #create sample customer
-def create_finicity_customer(token, api_key, username, first_name, last_name):
+def create_finicity_customer(app_key, app_token, username):
     url = 'https://api.finicity.com/aggregation/v2/customers/testing'
 
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Finicity-App-Key': api_key,
-        'Finicity-App-Token': token,
+        'Finicity-App-Key': app_key,
+        'Finicity-App-Token': app_token
     }
 
     data = {
-        "username": username,
-        "firstName": first_name,
-        "lastName": last_name
+        "username": username
     }
 
     response = requests.post(url, headers=headers, json=data)
 
-    if response.status_code == 200:
-        customer_info = response.json()
-        return customer_info
+    if response.status_code == 200 or response.status_code == 201:
+        print("Request successful.")
+        print("Response:", response.json())
+        return response.json()
     else:
-        print(f'Error: Unable to create customer. Status code: {response.status_code}')
-        return None
+        print(f"Request failed with status code {response.status_code}.")
+        print("Response:", response.text)
+    return 0
+    
+
 
 
 #generate connect link
@@ -72,9 +80,9 @@ def generate_finicity_token(api_token, api_key, partner_id, customer_id):
         "partnerId": partner_id,
         "customerId": customer_id
     }
-
+    
     response = requests.post(url, headers=headers, json=data)
-
+    
     if response.status_code == 200:
         token_info = response.json()
         return token_info
@@ -101,3 +109,22 @@ def get_finicity_accounts(api_token, api_key, customer_id):
     else:
         print(f'Error: Unable to fetch accounts data. Status code: {response.status_code}')
         return None
+
+
+#combine all into one step
+#generate_finicity_token(api_token, api_key, partner_id, customer_id):
+
+def generate_sample_customer(key, id, secret, url, username):
+    token = getToken(key, secret, id, url)
+    customer_data = create_finicity_customer(key, token['token'], username)
+
+    print(token['token'])
+    print(key)
+    print(id)
+    print(customer_data['id'])
+
+    link = generate_finicity_token(token['token'], key, id, customer_data['id'])
+    print(link)
+    
+
+generate_sample_customer(API_KEY, partnerId, partnerSecret, url, 'customer25_2023-10-14')
